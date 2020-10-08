01497c77f89f348c07c545b26fae50b53f453a76
Akshat Zala <akshatzala@gmail.com>
2020-10-08 14:53:57 +0530

    Added navigation bar

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ id, name, price, imageUrl}) => (
   <div className='collection-item'>
-    <div
-      className='image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
+    <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
+    
     <div className='collection-footer'>
       <span className='name'>{name}</span>
       <span className='price'>{price}</span>
