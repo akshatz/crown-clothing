e7191f6ef01a64ab287eaffb27e4a5294914d275
Akshat Zala <akshatzala@gmail.com>
2020-10-12 19:16:11 +0530

    Signed in successfully with firebase provider google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,8 +4,8 @@ import './sign-in.styles.scss';
 
 import FormInput from '../form-input/form-input.component.jsx'
 import CustomButton from '../custom-button/custom-button.component.jsx'
-import {signInWithGoogle} from '../../firebase/firebase.utils.js';
 
+import  { signInWithGoogle } from '../../firebase/firebase.utils.js';
 class Signin extends Component{
     constructor(props){
         super(props);
@@ -13,7 +13,7 @@ class Signin extends Component{
     this.state = {
         email: '',
         password: ''
-    }
+    };
     }
     
     handleSubmit = event => {
@@ -35,26 +35,25 @@ class Signin extends Component{
                 <span>Sign in with email & password </span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <label>Email</label>
                     <FormInput
-                     name="email" 
+                     name="email"
                      type="email" 
                      handleChange={this.handleChange} 
                      value={this.state.email} 
+                     label='Email'
                      required
                     />
-                    <label>Password</label>&nbsp;
                     <FormInput
                      name="password"
                      type="password"
-                     value={this.state.password} 
-                     handleChange={this.handleChange}
-                     required 
+                     value={this.state.password}
+                     handleChange={this.handleChange} 
+                     label='Password'
+                     required
                     />
-
-                    <CustomButton type='submit'>Submit</CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>
-                        Sign in with Google{''}
+                    <CustomButton type='submit'>Submit</CustomButton>&nbsp;
+                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                    Sign in with Google
                     </CustomButton>
                 </form> 
             </div>
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,10 +16,11 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+// export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const signInWithGoogle = () =>  auth.signInWithPopup(provider);
+
 
 export default firebase;
