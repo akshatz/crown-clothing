e4bd1a8b806c8912d9e2b28fe3bb8155c86004d3
Akshat Zala <akshatzala@gmail.com>
2020-10-14 18:59:49 +0530

    Added hidden to cart

--- src/redux/root-reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import userReducer from './user/user.reduer.jsx';
-
-export default combineReducers({
-  user: userReducer
-});
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,4 +1,6 @@
+import { UserActionTypes } from './user.types.js';
+
 export const setCurrentUser = user => ({
-    type: 'SET_CURRENT_USER',
-    payload: user
-  });
+  type: UserActionTypes.SET_CURRENT_USER,
+  payload: user
+});
