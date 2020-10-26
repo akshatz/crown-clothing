5652214d06ead12888099226f1236f05577706b6
Akshat Zala <akshatzala@gmail.com>
2020-10-26 17:09:04 +0530

    Added styled-components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -27,7 +27,7 @@ const CartDropdown = ({ cartItems, history, dispatch }) => (
         dispatch(toggleCartHidden());
       }}
     >
-      GO TO CHECKOUT
+      GO TO CART
     </CustomButton>
   </div>
 );
